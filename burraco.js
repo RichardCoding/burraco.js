39b248401930d23afe1ecc0ace40250ba8053aa4
Richard Berbeglia <31547104+RichardCoding@users.noreply.github.com>
2017-09-01 22:45:50 +0200

    Added the count of point for the "pozzetto", and written help.
    Added the count of point for the "pozzetto", and written help for the user.

--- burraco.js
+++ burraco.js
@@ -39,6 +39,7 @@ var burrachi_puliti;
 var burrachi_sporchi;
 var chiusura;
 var totale;
+var pozzetto = 0;
 
 // 15 punti
 var asso = "1";
@@ -101,7 +102,6 @@ function setup() {
   altezza = height;
 }
 
-
 function draw() {
   // colore carta = 192,192,192
   background(0,102,51);
@@ -126,10 +126,14 @@ function draw() {
     fill(0)
     stroke(0);
     text(asso,87,100);
-    text(asso,235,100);
+    text(asso,240,100);
     text(asso,87,334);
-    text(asso,235,334);
-    text(asso,160,215);
+    text(asso,240,334);
+    text(asso,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 2) {
     fill(0)
     stroke(0);
@@ -137,7 +141,11 @@ function draw() {
     text(tre,235,100);
     text(tre,87,334);
     text(tre,235,334);
-    text(tre,160,215);
+    text(tre,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 3) {
     fill(0)
     stroke(0);
@@ -145,7 +153,11 @@ function draw() {
     text(quattro,235,100);
     text(quattro,87,334);
     text(quattro,235,334);
-    text(quattro,160,215);
+    text(quattro,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 4) {
     fill(0)
     stroke(0);
@@ -153,7 +165,11 @@ function draw() {
     text(cinque,235,100);
     text(cinque,87,334);
     text(cinque,235,334);
-    text(cinque,160,215);
+    text(cinque,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 5) {
     fill(0)
     stroke(0);
@@ -161,7 +177,11 @@ function draw() {
     text(sei,235,100);
     text(sei,87,334);
     text(sei,235,334);
-    text(sei,160,215);
+    text(sei,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 6) {
     fill(0)
     stroke(0);
@@ -169,7 +189,11 @@ function draw() {
     text(sette,235,100);
     text(sette,87,334);
     text(sette,235,334);
-    text(sette,160,215);
+    text(sette,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 7) {
     fill(0)
     stroke(0);
@@ -177,7 +201,11 @@ function draw() {
     text(otto,235,100);
     text(otto,87,334);
     text(otto,235,334);
-    text(otto,160,215);
+    text(otto,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 8) {
     fill(0)
     stroke(0);
@@ -185,15 +213,23 @@ function draw() {
     text(nove,235,100);
     text(nove,87,334);
     text(nove,235,334);
-    text(nove,160,215);
+    text(nove,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 9) {
     fill(0)
     stroke(0);
     text(dieci,87,100);
-    text(dieci,208,100);
+    text(dieci,220,100);
     text(dieci,87,334);
-    text(dieci,208,334);
-    text(dieci,160,215);
+    text(dieci,220,334);
+    text(dieci,155,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 10) {
     fill(0)
     stroke(0);
@@ -201,39 +237,59 @@ function draw() {
     text(jack,235,100);
     text(jack,87,334);
     text(jack,235,334);
-    text(jack,160,215);
+    text(jack,165,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 11) {
     fill(0)
     stroke(0);
     text(donna,87,100);
-    text(donna,220,100);
+    text(donna,235,100);
     text(donna,87,334);
-    text(donna,220,334);
-    text(donna,160,215);
+    text(donna,235,334);
+    text(donna,165,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 12) {
     fill(0)
     stroke(0);
     text(re,87,100);
-    text(re,225,100);
+    text(re,235,100);
     text(re,87,334);
-    text(re,225,334);
-    text(re,160,215);
+    text(re,235,334);
+    text(re,165,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 13) {
     fill(0)
     stroke(0);
     text(pinella,87,100);
-    text(pinella,235,100);
+    text(pinella,240,100);
     text(pinella,87,334);
-    text(pinella,235,334);
-    text(pinella,160,215);
+    text(pinella,240,334);
+    text(pinella,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 14) {
     fill(0)
     stroke(0);
     text(jolly,87,100);
-    text(jolly,205,100);
+    text(jolly,225,100);
     text(jolly,87,334);
-    text(jolly,205,334);
+    text(jolly,225,334);
     text(jolly,160,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 15) {
 
     // Pezza Verde:
@@ -244,13 +300,13 @@ function draw() {
     // Box burraco:
     fill(200,200,200);
     stroke(200,200,200);
-    rect(90,80,435,200,10);
+    rect(75,80,435,200,10);
 
     // Scritta:
     fill(0);
     stroke(0);
     textFont("Arial",[64]);
-    text("Burraco Pulito.",100,200);
+    text("Burraco Pulito.",85,200);
 
   } else if (numerino == 16) {
 
@@ -262,13 +318,13 @@ function draw() {
     // Box burraco:
     fill(200,200,200);
     stroke(200,200,200);
-    rect(90,80,435,200,10);
+    rect(75,80,435,200,10);
 
     // Scritta:
     fill(0);
     stroke(0);
     textFont("Arial",[58]);
-    text("Burraco Sporco.",100,200);
+    text("Burraco Sporco.",85,200);
 
   } else if (numerino == 17) {
 
@@ -280,126 +336,200 @@ function draw() {
     // Box burraco:
     fill(200,200,200);
     stroke(200,200,200);
-    rect(90,80,435,200,10);
+    rect(75,80,435,200,10);
 
     // Scritta:
     fill(0);
     stroke(0);
     textFont("Arial",[75]);
-    text("Chiusura?",135,200);
+    text("Pozzetto?",120,200);
 
   } else if (numerino == 18) {
+
+    // Pezza Verde:
+    fill(0,102,51)
+    stroke(0,102,51)
+    rect(75,60,200,295,10);
+
+    // Box burraco:
+    fill(200,200,200);
+    stroke(200,200,200);
+    rect(75,80,435,200,10);
+
+    // Scritta:
     fill(0);
     stroke(0);
-    text(asso,87,100);
-    text(asso,205,100);
-    text(asso,87,334);
-    text(asso,205,334);
-    text(asso,160,215);
+    textFont("Arial",[75]);
+    text("Chiusura?",120,200);
+
   } else if (numerino == 19) {
     fill(0);
     stroke(0);
-    text(tre,87,100);
-    text(tre,205,100);
-    text(tre,87,334);
-    text(tre,205,334);
-    text(tre,160,215);
+    text(asso,87,100);
+    text(asso,240,100);
+    text(asso,87,334);
+    text(asso,240,334);
+    text(asso,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in mano:",10,35);
   } else if (numerino == 20) {
     fill(0);
     stroke(0);
-    text(quattro,87,100);
-    text(quattro,205,100);
-    text(quattro,87,334);
-    text(quattro,205,334);
-    text(quattro,160,215);
+    text(tre,87,100);
+    text(tre,235,100);
+    text(tre,87,334);
+    text(tre,235,334);
+    text(tre,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 21) {
     fill(0);
     stroke(0);
-    text(cinque,87,100);
-    text(cinque,205,100);
-    text(cinque,87,334);
-    text(cinque,205,334);
-    text(cinque,160,215);
+    text(quattro,87,100);
+    text(quattro,235,100);
+    text(quattro,87,334);
+    text(quattro,235,334);
+    text(quattro,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 22) {
     fill(0);
     stroke(0);
-    text(sei,87,100);
-    text(sei,205,100);
-    text(sei,87,334);
-    text(sei,205,334);
-    text(sei,160,215);
+    text(cinque,87,100);
+    text(cinque,235,100);
+    text(cinque,87,334);
+    text(cinque,235,334);
+    text(cinque,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 23) {
     fill(0);
     stroke(0);
-    text(sette,87,100);
-    text(sette,205,100);
-    text(sette,87,334);
-    text(sette,205,334);
-    text(sette,160,215);
+    text(sei,87,100);
+    text(sei,235,100);
+    text(sei,87,334);
+    text(sei,235,334);
+    text(sei,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 24) {
     fill(0);
     stroke(0);
-    text(otto,87,100);
-    text(otto,205,100);
-    text(otto,87,334);
-    text(otto,205,334);
-    text(otto,160,215);
+    text(sette,87,100);
+    text(sette,235,100);
+    text(sette,87,334);
+    text(sette,235,334);
+    text(sette,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 25) {
     fill(0);
     stroke(0);
-    text(nove,87,100);
-    text(nove,205,100);
-    text(nove,87,334);
-    text(nove,205,334);
-    text(nove,160,215);
+    text(otto,87,100);
+    text(otto,235,100);
+    text(otto,87,334);
+    text(otto,235,334);
+    text(otto,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 26) {
     fill(0);
     stroke(0);
-    text(dieci,87,100);
-    text(dieci,205,100);
-    text(dieci,87,334);
-    text(dieci,205,334);
-    text(dieci,160,215);
+    text(nove,87,100);
+    text(nove,235,100);
+    text(nove,87,334);
+    text(nove,235,334);
+    text(nove,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 27) {
     fill(0);
     stroke(0);
-    text(jack,87,100);
-    text(jack,205,100);
-    text(jack,87,334);
-    text(jack,205,334);
-    text(jack,160,215);
+    text(dieci,87,100);
+    text(dieci,220,100);
+    text(dieci,87,334);
+    text(dieci,220,334);
+    text(dieci,155,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 28) {
     fill(0);
     stroke(0);
-    text(donna,87,100);
-    text(donna,205,100);
-    text(donna,87,334);
-    text(donna,205,334);
-    text(donna,160,215);
+    text(jack,87,100);
+    text(jack,235,100);
+    text(jack,87,334);
+    text(jack,235,334);
+    text(jack,165,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 29) {
     fill(0);
     stroke(0);
-    text(re,87,100);
-    text(re,205,100);
-    text(re,87,334);
-    text(re,205,334);
-    text(re,160,215);
+    text(donna,87,100);
+    text(donna,235,100);
+    text(donna,87,334);
+    text(donna,235,334);
+    text(donna,165,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 30) {
     fill(0);
     stroke(0);
-    text(pinella,87,100);
-    text(pinella,205,100);
-    text(pinella,87,334);
-    text(pinella,205,334);
-    text(pinella,160,215);
+    text(re,87,100);
+    text(re,235,100);
+    text(re,87,334);
+    text(re,235,334);
+    text(re,165,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 31) {
     fill(0);
     stroke(0);
+    text(pinella,87,100);
+    text(pinella,240,100);
+    text(pinella,87,334);
+    text(pinella,240,334);
+    text(pinella,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
+  } else if (numerino == 32) {
+    fill(0);
+    stroke(0);
     text(jolly,87,100);
-    text(jolly,205,100);
+    text(jolly,225,100);
     text(jolly,87,334);
-    text(jolly,205,334);
-    text(jolly,160,215);
+    text(jolly,225,334);
+    text(jolly,170,215);
+    fill(0);
+    stroke(0);
+    textFont("Arial",[28]);
+    text("Clicca quante volte hai questa carta in tavola:",10,35);
   } else if (numerino == 50) {
     punti_assi = assi * 15;
     punti_tre_sette = tre_sette * 5;
@@ -420,7 +550,7 @@ function draw() {
     textFont("Arial",[128])
     text(punti_totali,170,170);
 
-  } else if (numerino == 32) {
+  } else if (numerino == 33) {
     punti_assi = assi * 15;
     punti_tre_sette = tre_sette * 5;
     punti_otto_re = otto_re * 10;
@@ -436,9 +566,17 @@ function draw() {
     meno_otto_re_punti = meno_otto_re * 10;
     meno_pinella_punti = meno_pinella * 20;
     meno_jollys_punti = meno_jollys * 30;
+    
     //  In mano (da pagare)
     valore_carte = meno_assi_punti + meno_pinella_punti + meno_jollys_punti + meno_tre_sette_punti + meno_otto_re_punti;
-    punti_totalissimi = punti_totali - valore_carte - 100;
+    if (pozzetto == 0) {
+    
+      punti_totalissimi = punti_totali - valore_carte - 100 - 100;
+      //console.log("Pozzetto non preso!");
+    
+    } else if (pozzetto == 1) {
+      punti_totalissimi = punti_totali - valore_carte - 100;
+    }
     fill(0,102,51);
     stroke(0,102,51);
     rect(75,60,200,295,10);
@@ -453,7 +591,7 @@ function draw() {
   
 
 
-  } else if (numerino == 33) {
+  } else if (numerino == 34) {
     fill(0,102,51);
     stroke(0,102,51);
     rect(75,60,200,295,10);
@@ -503,19 +641,19 @@ if (mouseX >= 80 && mouseX <= 265 && mouseY >= 60 && mouseY <= 340) {
       carte += 1;
       //console.log(jollys);
 
-    } else if (numerino == 18) {
+    } else if (numerino == 19) {
       meno_assi += 1;
       console.log(meno_assi);
-    } else if (numerino == 19 || numerino == 20 || numerino == 21 || numerino == 22 || numerino == 23) {
+    } else if (numerino == 20 || numerino == 21 || numerino == 22 || numerino == 23 || numerino == 24) {
       meno_tre_sette += 1;
       console.log(meno_tre_sette)
-    } else if (numerino == 24 || numerino == 25 || numerino == 26 || numerino == 27 || numerino == 28 || numerino == 29) {
+    } else if (numerino == 25 || numerino == 26 || numerino == 27 || numerino == 28 || numerino == 29 || numerino == 30) {
       meno_otto_re += 1;
       console.log(meno_otto_re);
-    } else if (numerino == 30) {
+    } else if (numerino == 31) {
       meno_pinella += 1;
       console.log(meno_pinella);
-    } else if (numerino == 31) {
+    } else if (numerino == 32) {
       meno_jollys += 1;
       console.log(meno_jollys);
     };
@@ -525,18 +663,20 @@ if (mouseX >= 80 && mouseX <= 265 && mouseY >= 60 && mouseY <= 340) {
 
     // TASTO AVANTI
   } else if (mouseX >= 390 && mouseX <= 570 && mouseY >= 320 && mouseY <= 370) {
-    if (numerino < 17 || numerino >= 18 && numerino < 33)  {
+    if (numerino < 18 || numerino >= 19 && numerino < 34)  {
       numerino += 1;
-    } else if (numerino = 17) {
+    } else if (numerino = 18) {
 
-      if (numerino == 17 && chiusura == 1) {
+      if (numerino == 18 && chiusura == 1) {
         punti_chiusura = 100;
         numerino = 50;
         console.log(numerino);
-      } else if (numerino == 17 && chiusura == 0) {
+      } else if (numerino == 18 && chiusura == 0) {
         numerino += 1;
         console.log("A", numerino);
-      };
+      } else if (numerino == 17) {
+        numerino += 1;
+      }
 
     }
 
@@ -555,12 +695,16 @@ if (mouseX >= 80 && mouseX <= 265 && mouseY >= 60 && mouseY <= 340) {
       burrachi_sporchi += 1;
       //console.log(burrachi_sporchi);
 
-    } else if (numerino == 17) {
+    } else if (numerino == 18) {
 
       chiusura += 1;
       console.log(chiusura);
 
-    }
+    } else if (numerino == 17) {
+
+      pozzetto += 1;
+      console.log("Hai preso il pozzetto!", pozzetto);
+    };
 
   } else {
 
